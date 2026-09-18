bbc108eb9ff4012316ba7187e1593e32cd9e16dc
Harsh Sharma <ichbinharsh@gmail.com>
2026-09-18 20:59:04 +0200

    new function added for age

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print_age(age):
+    print(f"I am {age} years old")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_age(38)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
