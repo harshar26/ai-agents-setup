a238f1d64f22e6715677efddde627e7898e23c4f
Harsh Sharma <ichbinharsh@gmail.com>
2026-09-18 21:03:34 +0200

    added function for kids number

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ def print_age(age):
     print(f"I am {age} years old")
 
 
+def print_kids(kids_num):
+    print(f"I have {kids_num} children.")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_age(38)
+    print_kids(1)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
